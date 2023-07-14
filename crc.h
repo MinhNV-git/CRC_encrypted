d043b2cc20c4f9c086ebf74605b840fd26a21e6e
MinhNV <minhnvw@gmail.com>
2023-07-14 10:33:59 +0900

    update test

--- crc.h
+++ crc.h
@@ -29,7 +29,7 @@ void display_CRC_16_table();
 
 void InitLockupTable_8(uint8_t);
 void InitLockupTable_16(uint16_t);
-// void InitLockupTable_32(uint32_t);
+void InitLockupTable_32(uint32_t);
 
 uint8_t Calculator_CRC8_table(uint8_t* input, uint16_t size, CRC_cfg_t cfg);
 uint8_t Calculator_CRC8_sw(uint8_t* input, uint16_t size, CRC_cfg_t cfg);
